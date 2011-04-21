6f2fb38e1a813360e6f71ef57392763e818a2538
Han He <upwell@gmail.com>
2011-04-21 04:51:30 -0500

    correct the install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
-if [ "$1" == "" ]; then
+if [ "xx$1" = "xx" ]; then
   echo ""
   echo "Usage to install amix's vimrc:"
   echo "   sh .vim_runtime/install.sh <system>"
   echo "      - where <system> can be 'mac', 'linux' or 'windows'"
   exit 1
 fi
-echo '
+echo "
 fun! MySys()
-   return "$1"
+   return \"$1\"
 endfun
 set runtimepath=~/.vim_runtime,~/.vim_runtime/after,\$VIMRUNTIME
 source ~/.vim_runtime/vimrc
-helptags ~/.vim_runtime/doc' > ~/.vimrc
+helptags ~/.vim_runtime/doc" > ~/.vimrc
 echo "Installed amix's vim configuration successfully! Enjoy :)"
